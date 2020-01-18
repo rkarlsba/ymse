d5c5577adeea6929f5a62eb796987ae00345e078
root <root@debian-buster-mal.karlsbakk.net>
2020-01-18 17:59:23 +0100

    der

--- mqtt/js/app_mqtt.js
+++ mqtt/js/app_mqtt.js
@@ -1,31 +1,33 @@
 // vim:ts=4:sw=4:sts=4:et:ai
+//
+const { Pool, Client } = require('pg');
+var format = require('pg-format');
 
-var pgsql = require('pgsql');
+const client = new Client({
+    user: 'mqtt',
+    host: 'localhost',
+    database: 'mqtt',
+    password: 'yaituaC1bengie0r',
+    port: 5432,
+});
+client.connect();
 
-// Create Connection
-// (yeah, I know I shouldn't post passwords on github, but this is for internal testing)
-var connection = pgsql.createConnection({
-    host: "localhost",
-    user: "mqtt",
-    password: "yaituaC1bengie0r",
-    database: "mqtt"
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+//  client.end();
+})
+
+var clientID= "mqtt.karlsbakk.net";
+var topic = "test/kitchen";
+var message = "dev01,on";
+
+var sqltemplate = "INSERT INTO %s(%s,%s,%s) VALUES ('%s', '%s', '%s')";
+var sql = format(sqltemplate, 'mqtt_messages', 'clientid', 'topic', 'message', clientID, topic, message);
+
+console.log(sql);
+client.query(sql, function (error, results) {
+    if (error) throw error;
+    console.log("1 record inserted");
+    client.end();
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Database Connected!");
-});
-
-//insert a row into the mqtt_messages table
-connection.connect(function(err) {
-    var clientID= "mqtt.karlsbakk.net";
-    var topic = "test/kitchen";
-    var message = "dev01,on";
-    var sql = "INSERT INTO ?? (??,??,??) VALUES (?,?,?)";
-    var params = ['mqtt_messages', 'clientid', 'topic', 'message', clientID, topic, message];
-    sql = pgsql.format(sql, params);
-    connection.query(sql, function (error, results) {
-        if (error) throw error;
-        console.log("1 record inserted");
-    });
-});
