99e7f1ad9c628af936e9e1d001891de71e62428d
root <root@debian-buster-mal.karlsbakk.net>
2020-01-18 18:09:21 +0100

    more

--- mqtt/js/app_mqtt.js
+++ mqtt/js/app_mqtt.js
@@ -1,22 +1,73 @@
 #!/usr/bin/node
 // vim:ts=4:sw=4:sts=4:et:ai
 //
-const { Pool, Client } = require('pg');
+const { PgClient } = require('pg');
 var format = require('pg-format');
+var mqtt = require('mqtt');
 
-const client = new Client({
+var Topic = '#'; //subscribe to all topics
+var Broker_URL = 'mqtt://mqtt.karlsbakk.net';
+
+var options = {
+    clientId: 'MyMQTT',
+    port: 1883,
+    keepalive : 60
+};
+
+const pgclient = new PgClient({
     user: 'mqtt',
     host: 'localhost',
     database: 'mqtt',
     password: 'yaituaC1bengie0r',
     port: 5432,
 });
-client.connect();
+pgclient.connect();
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-//  client.end();
-})
+var client = mqtt.connect(Broker_URL, options);
+client.on('connect', mqtt_connect);
+client.on('reconnect', mqtt_reconnect);
+client.on('error', mqtt_error);
+client.on('message', mqtt_messsageReceived);
+client.on('close', mqtt_close);
+
+function mqtt_connect() {
+    console.log("Connecting MQTT");
+    client.subscribe(Topic, mqtt_subscribe);
+}
+
+function mqtt_subscribe(err, granted) {
+    console.log("Subscribed to " + Topic);
+    if (err) {
+        console.log(err);
+    }
+}
+
+function mqtt_reconnect(err) {
+    console.log("Reconnect MQTT");
+    if (err) {
+        console.log(err);
+    }
+    client = mqtt.connect(Broker_URL, options);
+}
+
+function mqtt_error(err) {
+    console.log("Error!");
+    if (err) {
+        console.log(err);
+    }
+}
+
+function after_publish() {
+//do nothing
+}
+
+function mqtt_messsageReceived(topic, message, packet) {
+    console.log('Topic=' + topic + ' Message=' + message);
+}
+
+function mqtt_close() {
+    console.log("Close MQTT");
+}
 
 var clientID= "mqtt.karlsbakk.net";
 var topic = "test/kitchen";
@@ -26,9 +77,9 @@ var sqltemplate = "INSERT INTO %s(%s,%s,%s) VALUES ('%s', '%s', '%s')";
 var sql = format(sqltemplate, 'mqtt_messages', 'clientid', 'topic', 'message', clientID, topic, message);
 
 console.log(sql);
-client.query(sql, function (error, results) {
+pgclient.query(sql, function (error, results) {
     if (error) throw error;
     console.log("1 record inserted");
-    client.end();
+    pgclient.end();
 });
 
