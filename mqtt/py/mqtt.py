4f4afac5f63b31881fa32d8ef1e7cbe45581d874
root <root@debian-buster-mal.karlsbakk.net>
2020-01-19 22:13:50 +0100

    vel

--- mqtt/py/mqtt.py
+++ mqtt/py/mqtt.py
@@ -19,26 +19,31 @@
 #     print("message retain flag=",message.retain)
 # }}}
 
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt
 import time
-############
-def on_message(client, userdata, message):
+
+mqtt_broker ="localhost" 
+topic="#"
+
+def on_my_mqtt_message(mqtt_client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
-########################################
-broker_address="192.168.1.184"
-#broker_address="iot.eclipse.org"
+
 print("creating new instance")
-client = mqtt.Client("P1") #create new instance
-client.on_message=on_message #attach function to callback
+mqtt_client = mqtt.Client(mqtt_broker) #create new instance
+mqtt_client.on_mqtt_message=on_my_mqtt_message #attach function to callback
+
 print("connecting to broker")
-client.connect(broker_address) #connect to broker
-client.loop_start() #start the loop
-print("Subscribing to topic","house/bulbs/bulb1")
-client.subscribe("house/bulbs/bulb1")
-print("Publishing message to topic","house/bulbs/bulb1")
-client.publish("house/bulbs/bulb1","OFF")
-time.sleep(4) # wait
-client.loop_stop() #stop the loop
+mqtt_client.connect(mqtt_broker) #connect to broker
+mqtt_client.loop_start() #start the loop
+
+print("Subscribing to topic",topic)
+ret = mqtt_client.subscribe(topic, 0)
+print("Return value: ",ret)
+# print("Publishing message to topic",topic)
+# mqtt_client.publish(topic,"OFF")
+
+time.sleep(30) # wait
+mqtt_client.loop_stop() #stop the loop
