d5f481c2ea70dd4b2004bcb0491e625293e5d94a
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2020-01-19 22:46:13 +0100

    vel

--- mqtt/py/mqtt.py
+++ mqtt/py/mqtt.py
@@ -2,6 +2,8 @@
 # vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
 
 import paho.mqtt.client as mqtt
+import psycopg2
+from config import config
 
 mqtt_broker = "localhost"
 
@@ -12,9 +14,46 @@ def on_connect(mqttc, userdata, flags, rc):
 def on_message(mqttc, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-mqttc = mqtt.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-mqttc.connect(mqtt_broker)
+def pg_connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+ 
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+      
+        # create a cursor
+        cur = conn.cursor()
+        
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+ 
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+       # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+ 
+def do_mqtt():
+    mqttc = mqtt.Client()
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+    mqttc.connect(mqtt_broker)
 
-mqttc.loop_forever()
+    mqttc.loop_forever()
+     
+if __name__ == '__main__':
+    pg_connect()
+    do_mqtt()
+
