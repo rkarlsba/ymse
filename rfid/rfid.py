ccf658c355f6eff4fd8b6b533f3f30a56837de76
rkarlsba <roy@karlsbakk.net>
2020-02-06 16:55:48 +0100

    funkerlitt

---
+++ rfid/rfid.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# vim:ts=4:sw=4:sws=4:et:ai
+
+import sqlite3
+
+sqlitedb="tags.db"
+
+try:
+    conn=sqlite3.connect(sqlitedb)
+    c = conn.cursor()
+
+    c.execute('SELECT max(length(name)) FROM rfid_tags')
+    maxlength=c.fetchone()[0];
+
+    c.execute('SELECT name,id FROM rfid_tags ORDER BY name')
+    allrows = c.fetchall()
+    for row in allrows:
+        print(row[0].ljust(maxlength+1) + hex(row[1]))
+
+
+except sqlite3.Error as e:
+    print("An error occurred:", e.args[0])
+
+finally:
+    conn.close()
+
--- rfid/tags.sql
+++ rfid/tags.sql
@@ -1,8 +1,17 @@
--- CREATE TABLE rfid_tags(name varchar(20) not null, id int32, type varchar(20) not null, kb integer not null);
-INSERT INTO rfid_tags VALUES ('B1',0xc9c31087,'MIFARE Classic',1);
-INSERT INTO rfid_tags VALUES ('B2',0x27493234,'MIFARE Classic',1);
-INSERT INTO rfid_tags VALUES ('B3',0x4b892d1b,'MIFARE Classic',1);
-INSERT INTO rfid_tags VALUES ('K1',0x6954c083,'MIFARE Classic',1);
-INSERT INTO rfid_tags VALUES ('K2',0xfa760311,'MIFARE Classic',1);
-INSERT INTO rfid_tags VALUES ('K3',0x1ad90811,'MIFARE Classic',1);
-INSERT INTO rfid_tags VALUES ('Jobb',0x4d84d411,'MIFARE Classic',4);
+-- CREATE TABLE rfid_tags(name varchar(20) NOT NULL, id BIGINT, cardtype varchar(255) NOT NULL, kb integer);
+-- CREATE UNIQUE INDEX rfid_tags_name_idx ON rfid_tags(name);
+-- CREATE UNIQUE INDEX rfid_tags_id_idx ON rfid_tags(id);
+
+INSERT INTO rfid_tags(name,id,cardtype,kb) VALUES ('B1',0xc9c31087,'MIFARE Classic',1);
+INSERT INTO rfid_tags(name,id,cardtype,kb) VALUES ('B2',0x27493234,'MIFARE Classic',1);
+INSERT INTO rfid_tags(name,id,cardtype,kb) VALUES ('B3',0x4b892d1b,'MIFARE Classic',1);
+INSERT INTO rfid_tags(name,id,cardtype,kb) VALUES ('K1',0x6954c083,'MIFARE Classic',1);
+INSERT INTO rfid_tags(name,id,cardtype,kb) VALUES ('K2',0xfa760311,'MIFARE Classic',1);
+INSERT INTO rfid_tags(name,id,cardtype,kb) VALUES ('K3',0x1ad90811,'MIFARE Classic',1);
+INSERT INTO rfid_tags(name,id,cardtype,kb) VALUES ('Jobb',0x4d84d411,'MIFARE Classic',4);
+
+INSERT INTO rfid_tags(name,id,cardtype) VALUES ('L1',0x043de8329b6681,'MIFARE Ultralight or Ultralight C');
+INSERT INTO rfid_tags(name,id,cardtype) VALUES ('L2',0x0445e8329b6681,'MIFARE Ultralight or Ultralight C');
+INSERT INTO rfid_tags(name,id,cardtype) VALUES ('L3',0x0462e9329b6680,'MIFARE Ultralight or Ultralight C');
+INSERT INTO rfid_tags(name,id,cardtype) VALUES ('L4',0x042ce8329b6681,'MIFARE Ultralight or Ultralight C');
+INSERT INTO rfid_tags(name,id,cardtype) VALUES ('L5',0x0430e8329b6681,'MIFARE Ultralight or Ultralight C');
