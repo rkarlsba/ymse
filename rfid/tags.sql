6608c5ea7bb8466388b08ed170ed5f77e2ef302c
rkarlsba <roy@karlsbakk.net>
2020-02-06 15:48:05 +0100

    dill

---
+++ rfid/tags.sql
@@ -0,0 +1,8 @@
+-- CREATE TABLE rfid_tags(name varchar(20) not null, id int32, type varchar(20) not null, kb integer not null);
+INSERT INTO rfid_tags VALUES ('B1',0xc9c31087,'MIFARE Classic',1);
+INSERT INTO rfid_tags VALUES ('B2',0x27493234,'MIFARE Classic',1);
+INSERT INTO rfid_tags VALUES ('B3',0x4b892d1b,'MIFARE Classic',1);
+INSERT INTO rfid_tags VALUES ('K1',0x6954c083,'MIFARE Classic',1);
+INSERT INTO rfid_tags VALUES ('K2',0xfa760311,'MIFARE Classic',1);
+INSERT INTO rfid_tags VALUES ('K3',0x1ad90811,'MIFARE Classic',1);
+INSERT INTO rfid_tags VALUES ('Jobb',0x4d84d411,'MIFARE Classic',4);
