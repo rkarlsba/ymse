a04b051883b7858dba426b0fadc4bafd4bc7a5e6
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2020-02-08 17:06:26 +0100

    litt etter hvert

---
+++ rfid/py/example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (hex(id),text))
+        sleep(5)
+#except KeyboardInterrupt:
+finally:
+    GPIO.cleanup()
+    raise
