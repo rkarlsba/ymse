fcdcf30dc8d19c6643260e9781aa2849a6c97257
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2020-02-24 08:14:01 +0100

    mer moro i python

--- pi-temp-fan/py/pi-tempctl.py
+++ pi-temp-fan/py/pi-tempctl.py
@@ -2,6 +2,7 @@
 # vim:ts=4:sw=4:sts=4:et:ai
 
 import configparser
+import syslog
 
 # globals
 config_file = 'pi-tempctl.conf'
@@ -9,6 +10,10 @@ config_file = 'pi-tempctl.conf'
 # defaults
 c_temperaure_threashold = 45
 c_hysteresis_avoidance = 1
+c_fan_pin = 0;
+c_poll_rate = 1;
+
+syslog.openlog("pi-tempctl.py", logoption=syslog.LOG_PID, facility=syslog.LOG_DAEMON)
 
 # read config
 config = configparser.ConfigParser()
@@ -18,4 +23,8 @@ for key in config['default']:
         c_temperaure_threashold = config['default']['temperaure_threashold'];
     elif key == 'hysteresis_avoidance':
         c_hysteresis_avoidance = config['default']['hysteresis_avoidance'];
-print("Got temperaure_threashold {} and hysteresis_avoidance {}".format(c_temperaure_threashold, c_hysteresis_avoidance));
+
+syslog_message = "DEBUG: Got temperaure_threashold {} and hysteresis_avoidance {}".format(c_temperaure_threashold, c_hysteresis_avoidance);
+syslog.syslog(syslog.LOG_DEBUG, syslog_message);
+
+
