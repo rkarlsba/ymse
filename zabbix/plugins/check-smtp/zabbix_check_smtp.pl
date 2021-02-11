eafe096bcc176ff37d1fcaebbd2d1b8519f8255a
root <root@zabbix-dev.oslomet.no>
2021-02-11 13:30:55 +0100

    lala

--- zabbix/plugins/check-smtp/zabbix_check_smtp.pl
+++ zabbix/plugins/check-smtp/zabbix_check_smtp.pl
@@ -8,7 +8,7 @@ use Getopt::Long;
 
 # globals
 my $o_hostname;
-my $o_port = 25;
+my $o_port = undef;
 my $o_ssl = undef;
 my $o_timeout = 5;
 my $o_debug = 0;
@@ -28,10 +28,8 @@ Usage: zabbix_check_smtp.pl <-h host-name-or-IP> [-p port] [-s ssl|starttls ]
 
     --help        This text
 
-These options are all mutually exclusive.
-
     --hostname -h Name or IP address of host to check
-    --port -p     Port number [$o_port]
+    --port -p     Port number [not implemented]
     --ssl -s      Use SSL/TLS [none]
 
     --hello       Hostname to use for greeting [$o_hello]
@@ -50,14 +48,33 @@ GetOptions(
 );
 
 &help("Need hostname") unless (defined($o_hostname));
+if (defined($o_port)) {
+    warn("port number not implemented (yet)\n");
+}
 
-my $smtp = Net::SMTP->new(
-    $o_hostname,
-    Port => $o_port,
-    Timeout => $o_timeout,
-    Debug => $o_debug,
-    Hello => $o_hello,
-);
+my $use_ssl = 0;
+my $smtp;
+if (defined($o_ssl)) {
+    if ($o_ssl eq "ssl" or $o_ssl eq "starttls") {
+        $smtp = Net::SMTP->new(
+            $o_hostname,
+            Timeout => $o_timeout,
+            Debug => $o_debug,
+            Hello => $o_hello,
+            SSL => $o_ssl,
+        ) or die "Can't connect to $o_hostname: $@";;
+    } else {
+        print STDERR "ssl must be either ssl or starttls\n";
+        exit(1);
+    }
+} else {
+    $smtp = Net::SMTP->new(
+        $o_hostname,
+        Timeout => $o_timeout,
+        Debug => $o_debug,
+        Hello => $o_hello,
+    ) or die "Can't connect to $o_hostname: $@";;
+}
 unless ($smtp) {
     print STDERR "Error: ", $smtp->message();
     exit(1);
