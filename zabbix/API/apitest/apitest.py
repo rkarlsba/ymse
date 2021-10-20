a4ebac72ec3ff7c8a62e83f202ab0d3a8896fe44
Roy Sigurd Karlsbakk <roysk@hioa.no>
2021-10-20 12:25:52 +0200

    mere

--- zabbix/API/apitest/apitest.py
+++ zabbix/API/apitest/apitest.py
@@ -3,6 +3,7 @@
 
 from pyzabbix.api import ZabbixAPI
 import json
+import sys
 from local_passwords import api_user, api_password, api_url
 
 '''
@@ -30,22 +31,28 @@ funker dette?
 }
 '''
 
-# Create ZabbixAPI class instance
-zapi = ZabbixAPI(url=api_url, user=api_user, password=api_password)
+try:
+    # Create ZabbixAPI class instance
+    zapi = ZabbixAPI(url=api_url, user=api_user, password=api_password)
+    # Get all monitored hosts
+    allhosts = zapi.host.get(output='extend')
 
-# Get all monitored hosts
-result1 = zapi.host.get(output='extend')
+    print(json.dumps(allhosts,indent=4))
 
-hostnames1 = [host['host'] for host in result1]
-for host in result1:
-    if host['inventory_mode'] == "-1":
-        #print(json.dumps(host,indent=4))
-        host_update_json = {
-            "hostid": host['hostid'],
-            "inventory_mode": 1
-        };
-        print(json.dumps(host_update_json,indent=4))
-        result2 = zapi.do_request('host.update', host_update_json)
-        #result2 = zapi.host.print(json.dumps(host,indent=4))
+    sys.exit()
 
-zapi.user.logout()
+    for host in allhosts:
+        if host['host'] == "roysk.oslomet.no":
+            print(json.dumps(host,indent=4))
+            #host_update_json = {
+            #    "hostid": host['hostid'],
+            #    "inventory_mode": 1
+            #};
+            #print(json.dumps(host_update_json,indent=4))
+            #result2 = zapi.do_request('host.update', host_update_json)
+            #result2 = zapi.host.print(json.dumps(host,indent=4))
+
+    zapi.user.logout()
+except ZabbixAPIException as e:
+    print("Some Zabbix API error: {}".format(d))
+
