bc99322e7f66ca0eb5b8d3d784cd97f0e5027a21
Roy Sigurd Karlsbakk <roysk@hioa.no>
2021-10-20 10:12:04 +0200

    lala

--- zabbix/API/apitest/apitest.py
+++ zabbix/API/apitest/apitest.py
@@ -3,7 +3,7 @@
 
 from pyzabbix.api import ZabbixAPI
 import json
-import local_passwords
+from local_passwords import api_user, api_password, api_url
 
 # Create ZabbixAPI class instance
 zapi = ZabbixAPI(url=api_url, user=api_user, password=api_password)
@@ -14,7 +14,10 @@ result1 = zapi.host.get(hostid=10194, output='extend')
 hostnames1 = [host['host'] for host in result1]
 #hostnames1 = [inventory_mode['host'] for host in result1]
 
-print(json.dumps(result1, indent=4))
+#print(json.dumps(result1, indent=4))
 #print(result1)
+#print(hostnames1)
+for host in result1:
+    print('{} {}'.format(host['host'], host['inventory_mode']))
 
 zapi.user.logout()
