6a1d5a5eb857f8658713675140a919fcee19d59c
root <root@zabbix.oslomet.no>
2021-12-15 10:56:49 +0100

    oversatt til engelsk

--- zabbix/plugins/check-log4j/check_log4j.sh
+++ zabbix/plugins/check-log4j/check_log4j.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # vim:ts=4:sw=4:sts=4:et:ai
 
-# Søk etter log4j i alle jar-filer. Forutsetter at locate-databasen er 
-# oppdatert. På grunn av problematikk med mellomrom i katalog og filnavn 
-# legges listen over jar-filer i en temporær fil.
-
-# se man mktemp (eller trykk K på kommandoen her i vim for å fyre opp manuelen for den)
+# See mktemp(1)
 MKTMPTEMP="/tmp/log4jsearch.XXXXX"
 REGEX="log4j\.jar|log4j-[0-9].*\.jar$|log4j-core.*\.jar$"
 OUTFILE=$( mktemp $MKTMPTEMP )
@@ -14,30 +10,31 @@ QUIET=0
 HITS=0
 STATUS='OK'
 STATUSTEXT=''
+USE_LOCATE=0
+OS=$( uname )
 
-# Funksjon som kalles ved avslutning av skriptet.
+# This is called on exit
 function cleanup() {
     rm -f $OUTFILE $JARLIST
 }
 
-# Kall funksjonen cleanup når skriptet avsluttes, med mindre det får en SIGKILL - den når aldri skriptet.
+# Call this function on exit (except if we get SIGKILL, then we won't see it)
 trap cleanup EXIT
 
 if [ $UID -gt 0 ]
 then
-    if [ $QUIET -eq 0 ]
-    then
-        STATUS="WARNING"
-        STATUSTEXT="Running under a non-root user. This may not find all files. "
-    fi
+    STATUS="WARNING"
+    STATUSTEXT="Running under a non-root user. This may not find all files. "
 fi
 
-# Sjekk om vi har locate og bruk den hvis vi har den.
+# Check if we have locate
 if $( which locate > /dev/null 2>&1 )
 then
-    USE_LOCATE=1
-else
-    USE_LOCATE=0
+    # We need gnu locate, so we'll stick to checking if this is Linux or not
+    if [ '$OS' == 'Linux' ]
+    then
+        USE_LOCATE=1
+    fi
 fi
 
 if [ $USE_LOCATE -gt 0 ]
