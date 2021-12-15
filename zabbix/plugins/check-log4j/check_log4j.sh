b9a86ccde485befdf3634564f37365db049ef3dd
root <root@zabbix-dev.oslomet.no>
2021-12-15 10:23:38 +0100

    funker

--- zabbix/plugins/check-log4j/check_log4j.sh
+++ zabbix/plugins/check-log4j/check_log4j.sh
@@ -5,25 +5,58 @@
 # oppdatert. På grunn av problematikk med mellomrom i katalog og filnavn 
 # legges listen over jar-filer i en temporær fil.
 
-USE_LOCATE=1
+# se man mktemp (eller trykk K på kommandoen her i vim for å fyre opp manuelen for den)
+MKTMPTEMP="/tmp/log4jsearch.XXXXX"
+REGEX="log4j\.jar|log4j-[0-9].*\.jar$|log4j-core.*\.jar$"
+OUTFILE=$( mktemp $MKTMPTEMP )
+JARLIST=$( mktemp $MKTMPTEMP )
+QUIET=0
+HITS=0
 
-OUTFILE='/var/run/zabbix/zabbix_log4j.search'
+# Funksjon som kalles ved avslutning av skriptet.
+function cleanup() {
+    rm -f $OUTFILE $JARLIST
+}
+
+# Kall funksjonen cleanup når skriptet avsluttes, med mindre det får en SIGKILL - den når aldri skriptet.
+trap cleanup EXIT
+
+# Sjekk om vi har locate og bruk den hvis vi har den.
+if $( which locate > /dev/null 2>&1 )
+then
+    USE_LOCATE=1
+else
+    USE_LOCATE=0
+fi
 
 if [ $USE_LOCATE -gt 0 ]
 then
-locate -r "\.jar$" >/tmp/jars-$$
+    locate -r "\.jar$" > $JARLIST
+else
+    if [ $QUIET -eq 0 ]
+    then
+        echo "Warning: command 'locate' not found. Falling back to 'find', which may" >&2
+        echo "time out Zabbix checks." >&2
+    fi
+    find / -iname '*.jar' -print > $JARLIST
+fi
 
-# find / -name '*.jar' -print >/tmp/jars-$$ # Alternativ om locate mangler
-REGEX="log4j\.jar|log4j-[0-9].*\.jar$|log4j-core.*\.jar$"
 while read line
 do
-  HIT=`unzip -l "$line" 2>/dev/null | egrep -i "$REGEX"`
-  if [ -n "$HIT" ]; then
-    echo -e "$line\n$HIT\n"
-    HIT=""
-    echo -e "$line\n$HIT\n" > $OUTFILE
-  else
-    echo -e "0" > $OUTFILE
-  fi
-done < /tmp/jars-$$
-rm /tmp/jars-$$
+    HIT=`unzip -l "$line" 2>/dev/null | egrep -i "$REGEX"`
+    if [ -n "$HIT" ]; then
+        HITS=$( $HITS + 1 )
+        echo -e "$line\n$HIT\n" >> $OUTFILE
+    fi
+done < $JARLIST
+
+if [ $HITS -gt 0 ]
+then
+    echo "ERROR: Found a total of $HITS hits on log4j search:"
+    cat $OUTFILE
+    exit 1
+else
+    echo "OK: log4j not found on this machine"
+fi
+
+exit 0
