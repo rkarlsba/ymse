bef00c2a3f9792c565524b7c95a05e6641e110cf
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2022-01-19 13:30:43 +0100

    lala

--- sikkerhet/fix-cvs-2022-23222.pl
+++ sikkerhet/fix-cvs-2022-23222.pl
@@ -5,17 +5,18 @@ use strict;
 use warnings;
 
 # Stuff
-my $debug = 1;
+my $verbose = 1;
 
 # Fetch current value
 my $keyname = "kernel.unprivileged_bpf_disabled";
 my $unprivileged_bpf_disabled = `sysctl -n $keyname`;
-print "$unprivileged_bpf_disabled is $unprivileged_bpf_disabled." if ($debug -gt 0);
+chomp($unprivileged_bpf_disabled);
+print "unprivileged_bpf_disabled is $unprivileged_bpf_disabled.\n" if ($verbose gt 0);
 
 if ($unprivileged_bpf_disabled == 0) {
-	print "ERROR: Security hole wide open\n" if ($debug -gt 0);
+	print "ERROR: Security hole wide open\n" if ($verbose gt 0);
 } elsif ($unprivileged_bpf_disabled == 1) {
-	print "OK: Security hole firmly closed and can't be opened without a reboot\n" if ($debug -gt 0);
+	print "OK: Security hole firmly closed and can't be opened without a reboot\n" if ($verbose gt 0);
 } elsif ($unprivileged_bpf_disabled == 2) {
-	print "OK: Security hole closed, but can be open by setting $keyname to 0\n" if ($debug -gt 0);
+	print "OK: Security hole closed, but can be open by setting $keyname to 0\n" if ($verbose gt 0);
 }
