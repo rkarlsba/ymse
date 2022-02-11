4cd533b1a3c9422040130916259754e5b541a21f
root <root@oslomet.no>
2022-02-11 13:47:08 +0100

    asdf

--- zabbix/plugins/check-selinux/check_selinux.sh
+++ zabbix/plugins/check-selinux/check_selinux.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# vim:ts=4:sw=4:sts=4:et:ai
 
 selinuxstatus=$( getenforce )
 case $selinuxstatus in
-	Permissive)
-		;;
-		ZZ
+    Enforcing)
+        echo "OK: SELinux is enforcing security"
+        ;;
+    Permissive)
+        echo "WARNING: SELinux is set to permissive mode"
+        ;;
+    Disabled)
+        echo "ERROR: SELinux is disabled"
+        ;;
+    *)
+        echo "UNKNWON: getenforce returned $selinuxstatus of which I don't know a thing"
+        ;;
+esac
+
