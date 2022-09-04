5f1df2717435ae4f158ce134b0f9d6341cffaf0a
Roy Sigurd Karlsbakk <roysk@hioa.no>
2022-09-04 18:49:45 +0200

    funker denne?

--- vm-greier/mkvm/mkvm.sh
+++ vm-greier/mkvm/mkvm.sh
@@ -11,21 +11,45 @@
 #
 # Roy Sigurd Karlsbakk <roy@karlsbakk.net>, 2022
 #
-# Released under GPL v3 - see LICENSE for details
+# Released under GPL v3 - see LICENSE.md for details
 
 # 'Globals'
 TEMPLATE_VM='debian-bullseye-mal'
 DOMAIN='karlsbakk.net'
+ORIGINAL="$TEMPLATE_VM.$DOMAIN"
 
 # Our loop
 for vm in $@
 do
-    # Locals
-    virt-clone --original debian-bullseye-mal.karlsbakk.net --name prat.karlsbak.net --file prat.karlsbak.net-root1.qcow2
-    $_exitcode = $?
+    # 'Locals'
+    vm_name="$vm.$DOMAIN"
+    vm_file="$vm_name-root1.qcow2"
+
+    # Sanity check
+    if $( echo "$vm_name" | perl -ne 'exit 1 unless (/^[a-z0-9\-\.]+/);' )
+    then
+        echo "Illegal hostname: $vm_name, exiting"
+        exit 1
+    fi
+    if $( virsh domstate $ORIGINAL > /dev/null 2>&1 )
+    then
+        echo "VM template $ORIGINAL seems to exist, exiting"
+        exit 2
+    fi
+    if $( virsh domstate $vm_name > /dev/null 2>&1 )
+    then
+        echo "VM $vm_name seems to exist, exiting"
+        exit 3
+    fi
+
+    echo Cloning template VM $TEMPLATE_VM to new VM $vm_name
+
+    virt-clone --original $ORIGINAL --name $vm_name --file $vm_file
     if [ $_exitcode != 0 ]
     then
         echo 'Something terrible happened - exiting' >&2
         exit $_exitcode
+    else
+        echo 'Done'
     fi
 done
