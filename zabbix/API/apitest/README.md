256ba43c699d913017c3f979514a0b4f4d4fa27e
Roy Sigurd Karlsbakk <roysk@roysk.oslomet.no>
2022-09-05 13:06:46 +0200

    fiske

--- zabbix/API/apitest/README.md
+++ zabbix/API/apitest/README.md
@@ -16,16 +16,20 @@ in a versioning system like git or similar, for obvious reasons.
 
 ## `local_passwords.py`
 ```
-api_user = 'zabbixapiuser'
-api_password = 'supersecretpassword'
-api_url = 'https://zabbix.asdf.com/zabbix/api_jsonrpc.php';
+api_user = api_src_user = 'zabbixapiuser'
+api_password = api_src_password = 'supersecretpassword'
+api_url = api_src_url = 'https://zabbix.asdf.com/zabbix/api_jsonrpc.php';
+
+api_dst_user = 'zabbixapiuser'
+api_dst_password = 'supersecretpassword'
+api_dst_url = 'https://zabbix.asdf.com/zabbix/api_jsonrpc.php';
 ```
 
 Then import it with something like this:
 
 ```
 try:
-    from local_passwords import api_user, api_password, api_url
+    from local_passwords import *
 except:
     print("File local_passwords.py does not exists. Please refer to the README file", file=sys.stderr)
     print("and create the named file before running this again.", file=sys.stderr)
--- zabbix/API/apitest/migrate_user_group.py
+++ zabbix/API/apitest/migrate_user_group.py
@@ -6,9 +6,10 @@ import json
 import sys
 
 try:
-    from local_passwords import api_user, api_password, api_url
+    from local_passwords import *
+
 except:
-    print("File local_passwords.py does not exists. Please refer to the README file", file=sys.stderr)
+    print("File local_passwords.py does not exists. Please refer to the README.md file", file=sys.stderr)
     print("and create the named file before running this again.", file=sys.stderr)
     exit(1)
 
@@ -43,25 +44,28 @@ try:
     # Create ZabbixAPI class instance
     zapi = ZabbixAPI(url=api_url, user=api_user, password=api_password)
 
-    # Get all monitored hosts
-    allhosts = zapi.host.get(output='extend', selectInventory='extend')
+    # Get all users
+    allhosts = zapi.user.get(output='extend')
 
-    # print(json.dumps(allhosts,indent=4))
-
-    # sys.exit()
-
-    for host in allhosts:
-        if 'os' in host['inventory']:
-            if host['inventory']['os'] == '':
-                print("{} has no OS in its inventory".format(host['host']))
-            else:
-                print("{} has OS {}".format(host['host'], host['inventory']['os']))
-        if 0 and host['host'] == "roysk.oslomet.no":
-            print(json.dumps(host,indent=4))
-#           hlookup=zapi.host.get(output=['hostid'], selectInventory='extend', hostids=host['hostid'])
-            #print("URL A {}".format(hlookup['inventory']['url_a']))
-            #print(json.dumps(hlookup[0]['inventory']['url_a'],indent=4))
-            print(json.dumps(hlookup[0]['inventory'],indent=4))
+#     # Get all monitored hosts
+#     allhosts = zapi.host.get(output='extend', selectInventory='extend')
+# 
+#     # print(json.dumps(allhosts,indent=4))
+# 
+#     # sys.exit()
+# 
+#     for host in allhosts:
+#         if 'os' in host['inventory']:
+#             if host['inventory']['os'] == '':
+#                 print("{} has no OS in its inventory".format(host['host']))
+#             else:
+#                 print("{} has OS {}".format(host['host'], host['inventory']['os']))
+#         if 0 and host['host'] == "roysk.oslomet.no":
+#             print(json.dumps(host,indent=4))
+# #           hlookup=zapi.host.get(output=['hostid'], selectInventory='extend', hostids=host['hostid'])
+#             #print("URL A {}".format(hlookup['inventory']['url_a']))
+#             #print(json.dumps(hlookup[0]['inventory']['url_a'],indent=4))
+#             print(json.dumps(hlookup[0]['inventory'],indent=4))
 #           #host_update_json = {
 #           #    "hostid": host['hostid'],
 #           #    "inventory_mode": 1
