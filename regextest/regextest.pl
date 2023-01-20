b17025a2acc9a0559e98c604c327d93e04e8fe4c
Roy Sigurd Karlsbakk <roysk@hioa.no>
2023-01-20 17:54:26 +0100

    asd

--- regextest/regextest.pl
+++ regextest/regextest.pl
@@ -5,11 +5,18 @@ use strict;
 use warnings;
 
 my $infn = 'regextest.txt';
+my $inf;
+my $line;
+my $searchstring = 'Roy';
 
 open($inf, $infn) || die "Can't open input file $inf: $!\n";
 
-while (my $line = <$inf>) {
-    print;
+while ($line = <$inf>) {
+    chomp($line);
+    if ($line =~ /\b($searchstring)\b/) {
+        print "String \"$searchstring\" found in line \"$line\"\n";
+    }
+    #print("$line\n");
 }
 
 close($inf)
