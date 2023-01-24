9c482e01f926b448fd63b2cb8d24bf3cf66f8e96
Roy Sigurd Karlsbakk <roysk@hioa.no>
2023-01-24 23:33:13 +0100

    mere

--- ardugreier/espen/konfig/get-packages.sh
+++ ardugreier/espen/konfig/get-packages.sh
@@ -2,10 +2,11 @@
 # vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
 
 URL_FILE="urls.txt"
+PKG_DIR="packages"
 
 while IFS= read -r url
 do
     url=$( echo "$url" | perl -pe 's/^\s*#.*//' )
     [[ "$url" = "" ]] && continue
-    echo $url
+    wget --directory-prefix=$PKG_DIR $url
 done < $URL_FILE
