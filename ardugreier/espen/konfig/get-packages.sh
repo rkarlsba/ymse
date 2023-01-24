620a36b45490f00282a05d35210aa45b10f158c2
Roy Sigurd Karlsbakk <roysk@hioa.no>
2023-01-25 00:15:55 +0100

    ok?

--- ardugreier/espen/konfig/get-packages.sh
+++ ardugreier/espen/konfig/get-packages.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
+#
+# Written by Roy Sigurd Karlsbakk <roy@karlsbakk.net>
+# Licensed under the Apache license version 2.0. Please
+# see the LICENSE file for details.
 
 URL_FILE="urls.txt"
-PKG_DIR="packages"
+PKG_DIR="sadf"
 
 while IFS= read -r url
 do
     url=$( echo "$url" | perl -pe 's/^\s*#.*//' )
     [[ "$url" = "" ]] && continue
-    wget --directory-prefix=$PKG_DIR $url
+    wget --directory-prefix=$PKG_DIR --quiet $url
 done < $URL_FILE
