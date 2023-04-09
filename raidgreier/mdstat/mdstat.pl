4a5f477b25b3950c9e50ead59b9526e3e045b7b2
Roy Sigurd Karlsbakk <roysk@hioa.no>
2023-04-09 15:17:25 +0200

    funker

--- raidgreier/mdstat/mdstat.pl
+++ raidgreier/mdstat/mdstat.pl
@@ -4,14 +4,40 @@
 use strict;
 use warnings;
 
-my $debug = 1; # set to 0 for a normal run
+my $debug = 0; # set to 0 for a normal run
 my $fn = '/proc/mdstat';
+my $dev = shift;
+my $found=0;
+
+sub debprint {
+    my $s = shift;
+    $s = "¿qué?" unless (defined($s));
+    print("[DEBUG] $s") if ($debug);
+}
+
+die("Syntax: $0 mdX\n") unless (defined($dev));
+warn("Is '$dev' really an md device?\n") unless ($dev =~ /^md\d+$/);
 
 $fn = 'mdstat.txt' if ($debug);
 
-die("This must be run on Linux\n") if ($^O ne "Linux");
+die("This must be run on Linux\n") if ($^O ne "Linux" and not $debug);
 
 open(my $mdstat, "<", $fn) ||
     die("Failed to open file '$fn': $!\n");
 
+debprint("Looking for '$dev' in the haystack…\n");
+
+while (my $line = <$mdstat>) {
+    chomp($line);
+    debprint("Looking for line '$line'\n");
+    if ($line =~ /^${dev}\s/) {
+        $found=1;
+    }
+    if ($found) {
+        print "$line\n";
+        if ($line =~ /^\s*$/) {
+            $found=0;
+        }
+    }
+}
 
