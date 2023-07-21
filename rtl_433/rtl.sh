c4e08bfba3764f0eaf2b399e6b7a14dbcfa1ea98
rkarlsba <roy@karlsbakk.net>
2023-07-21 08:35:08 +0200

    rtl_433-skript

---
+++ rtl_433/rtl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+homedir='/data/ymse/rtl_433'
+#protocols='DEFAULT'
+protocols='ALL'
+#protocols='{1..10} {12,15}'
+verbose='1'
+logging='-F log -F kv -F json:rtl_433.json'
+sample_rate='-s 2048k'
+config='/etc/rtl_433.conf'
+exclude_from_all='10[67]'
+
+arg=''
+
+cd $homedir
+
+while [ $verbose -gt 0 ]
+do
+    arg+='-v '
+    verbose=$(( verbose - 1 ))
+done
+
+case $protocols in
+    DEFAULT)
+        ;;
+    ALL)
+        arg+=$( grep '^[# ]  *protocol ' $config | cut -c2- | awk '{ print "-R " $2 }' | egrep -v "$exclude_from_all" | tr '\n' ' ' )
+        ;;
+    *)
+        protocols=$( echo $protocols )
+        for p in $protocols
+        do
+            arg+="-R $p "
+        done
+        ;;
+esac
+rtl_433 $sample_rate $logging $arg
