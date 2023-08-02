f039fe0ef1c2c191294d62075678cf0f4aac2a37
root <root@sandkasse.oslomet.no>
2023-08-02 14:32:16 +0200

    funker

--- RedHat/disable-insights.sh
+++ RedHat/disable-insights.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 # vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
 
-INSIGHTS="insights-client-boot.service insights-client-results.path insights-client-results.service insights-client.service insights-client.timer"
+INSIGHTS="insights-client-boot.service insights-client-results.path insights-client.timer"
+DISABLE=1
+
+if [ "$#" -gt 0 ] && [ "$1" = "--enable" ]
+then
+    DISABLE=0
+fi
+
 for i in $INSIGHTS
 do
-    systemctl disable $i
-    systemctl mask $i
+    if [ "$DISABLE" -eq 0 ]
+    then
+        echo "enabling $i"
+        systemctl unmask $i
+        systemctl enable $i
+    else
+        systemctl disable $i
+        systemctl mask $i
+    fi
 done
