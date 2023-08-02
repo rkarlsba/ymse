73841f0e684045bd55242653461f49ca241bdb81
Roy Sigurd Karlsbakk <roysk@hioa.no>
2023-08-02 14:22:09 +0200

    asdf

--- psstat/psstat.pl
+++ psstat/psstat.pl
@@ -3,20 +3,16 @@
 
 use strict;
 use warnings;
-
-# se ps-axfv.txt for ps-output. Eksempel under
-#
-#   PID TTY      STAT   TIME  MAJFL   TRS   DRS   RSS %MEM COMMAND
-#     2 ?        S      0:00      0     0     0     0  0.0 [kthreadd]
-#     3 ?        I<     0:00      0     0     0     0  0.0  \_ [rcu_gp]
-#     4 ?        I<     0:00      0     0     0     0  0.0  \_ [rcu_par_gp]
+  use Getopt::Long;
 
 my %pstree;
 my $debug = 0;
 my $cmd = 'ps axfv';
+my $run = 'run';
 
-if ($^O != "linux") {
+if ($^O ne "linux") {
     $debug = 1;
+    $run = 'open';
     print(STDERR "Not sure about how to parse 'ps' output on $^O - switching to debug mode.\n");
 }
 
@@ -26,17 +22,25 @@ if ($debug gt 0) {
     $cmd = 'ps axfv';
 }
 
-open(my $cmd_fd, "$cmd|") ||
-    die("Can't run $cmd: $!\n");
+open(my $cmd_fd, "$cmd") ||
+    die("Can't $run '$cmd': $!\n");
 
 while (my $line = <$cmd_fd>) {
-    chomp($line);
-    print($line . "\n");
+#   chomp($line);
+#   print($line . "\n");
 #                     PID       TTY          STAT           TIME      MAJFL      TRS     DRS     RSS      %MEM      COMMAND
     if ($line =~ /\s*(\d+)\s+([\w\?\/]+)\s+([\w\<\+]+)\s+(\d+\:\d{2})\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+\.\d+)\s(.*)/) {
 #            1    2    3     4     5    
-        my ($pid,$tty,$stat,$time,$majfl,$trs,$drs,$rss,$pstmem,$command) = ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,
-        $pstree[
+        my ($pid,$tty,$stat,$time,$majfl,$trs,$drs,$rss,$pstmem,$command) = ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);
+        $pstree{$pid}{'tty'} = $tty;
+        $pstree{$pid}{'stat'} = $stat;
+        $pstree{$pid}{'time'} = $time;
+        $pstree{$pid}{'majfl'} = $majfl;
+        $pstree{$pid}{'trs'} = $trs;
+        $pstree{$pid}{'drs'} = $drs;
+        $pstree{$pid}{'rss'} = $rss;
+        $pstree{$pid}{'pstmem'} = $pstmem;
+        $pstree{$pid}{'command'} = $command;
     }
 }
 
