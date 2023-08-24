d13316a7d95e59b6c28c2a5aac8b824cff105a94
root <root@mail.karlsbakk.net>
2023-08-24 20:43:56 +0200

    asdf

--- backup/restic/restic-backup-post-exec.sh
+++ backup/restic/restic-backup-post-exec.sh
@@ -3,8 +3,19 @@
 # Do something smart, cleaning up temporary files from restic-backup-pre-exec.sh
 RESTIC_CONFIG='/etc/default/restic'
 
+# Defaults - overridden in user config
+export RESTIC_KEEP_DAILY=14
+export RESTIC_KEEP_WEEKLY=5
+export RESTIC_KEEP_MONTHLY=3
+export RESTIC_KEEP_YEARLY=0
+
 # Read config
 source $RESTIC_CONFIG
 
 # Clean up old backups
-restic forget --keep-daily $KEEP_DAILY --keep-weekly $KEEP_WEEKLY --keep-monthly $KEEP_MONTHLY --keep-yearly $KEEP_YEARLY --prune $@
+restic forget \
+	--keep-daily $RESTIC_KEEP_DAILY \
+	--keep-weekly $RESTIC_KEEP_WEEKLY \
+	--keep-monthly $RESTIC_KEEP_MONTHLY \
+	--keep-yearly $RESTIC_KEEP_YEARLY \
+	--prune $@
