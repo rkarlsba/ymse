3cd18b557373b1a36fe7bde3bc7b8cf1b73fc396
root <root@mangopi1.karlsbakk.net>
2023-11-01 21:24:38 +0100

    ikke daily, within

--- backup/restic/restic-backup-post-exec.sh
+++ backup/restic/restic-backup-post-exec.sh
@@ -4,7 +4,8 @@
 RESTIC_CONFIG='/etc/default/restic'
 
 # Defaults - overridden in user config
-export RESTIC_KEEP_DAILY=14
+# export RESTIC_KEEP_DAILY=14 # Daily not in use - 'within 14d' works better
+export RESTIC_KEEP_WIITHIN='14d'
 export RESTIC_KEEP_WEEKLY=5
 export RESTIC_KEEP_MONTHLY=3
 export RESTIC_KEEP_YEARLY=0
@@ -14,8 +15,8 @@ source $RESTIC_CONFIG
 
 # Clean up old backups
 restic forget \
-	--keep-daily $RESTIC_KEEP_DAILY \
+	--keep-within $RESTIC_KEEP_WIITHIN \
 	--keep-weekly $RESTIC_KEEP_WEEKLY \
 	--keep-monthly $RESTIC_KEEP_MONTHLY \
 	--keep-yearly $RESTIC_KEEP_YEARLY \
-	--prune $@
+	$RESTIC_PRUNE $@
