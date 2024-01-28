525c33e578ad51061aa6ac0a0827db771bc5cd40
rkarlsba <roy@karlsbakk.net>
2024-01-28 13:31:15 +0100

    fileliste - naa komplett - test.dat full av null og et skript, snart

---
+++ mp3analyse/mp3analyse.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+# vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
+
+use strict;
+use warnings;
+
+# {{{
+#
+# /data/empetre/filelist.txt is a list of my music files created with
+# find /data/empetre/ -type f -exec file + \; > /data/empetre/filetypes.txt
+#
+# }}}
