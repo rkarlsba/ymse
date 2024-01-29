89460f624ccd43d5b36b30877ef2de4c48078387
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2024-01-29 03:21:03 +0100

    ding

--- ymse/os-check.sh
+++ ymse/os-check.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "We're on linux!"
+    echo "We're on linux! ($OSTYPE)"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "We're on a mac!"
+    echo "We're on a mac! ($OSTYPE)"
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-    echo "We're on cygwin!"
+    echo "We're on cygwin! ($OSTYPE)"
 elif [[ "$OSTYPE" == "msys" ]]; then
-    echo "We're on MinGW?"
+    echo "We're on MinGW? ($OSTYPE)"
 elif [[ "$OSTYPE" == "win32" ]]; then
-    echo "We're on Win32???"
+    echo "We're on Win32??? ($OSTYPE)"
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    echo "We're on Freebsd <3"
+    echo "We're on Freebsd <3 ($OSTYPE)"
 else
     echo "We're on some freaky OS I don't know ($OSTYPE)"
 fi
