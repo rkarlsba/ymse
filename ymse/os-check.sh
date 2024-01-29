5c1da2428678bcdcc4af445b93fe1e6a343afd48
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2024-01-29 03:26:29 +0100

    oki

--- ymse/os-check.sh
+++ ymse/os-check.sh
@@ -24,20 +24,23 @@ case "$OSTYPE" in
         echo "We're on some freaky OS I don't know ($OSTYPE)"
         ;;
 esac
-exit
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "We're on linux! ($OSTYPE)"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "We're on a mac! ($OSTYPE)"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-    echo "We're on cygwin! ($OSTYPE)"
-elif [[ "$OSTYPE" == "msys" ]]; then
-    echo "We're on MinGW? ($OSTYPE)"
-elif [[ "$OSTYPE" == "win32" ]]; then
-    echo "We're on Win32??? ($OSTYPE)"
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    echo "We're on Freebsd <3 ($OSTYPE)"
-else
-    echo "We're on some freaky OS I don't know ($OSTYPE)"
-fi
+# Old code {{{
+#
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#     echo "We're on linux! ($OSTYPE)"
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#     echo "We're on a mac! ($OSTYPE)"
+# elif [[ "$OSTYPE" == "cygwin" ]]; then
+#     echo "We're on cygwin! ($OSTYPE)"
+# elif [[ "$OSTYPE" == "msys" ]]; then
+#     echo "We're on MinGW? ($OSTYPE)"
+# elif [[ "$OSTYPE" == "win32" ]]; then
+#     echo "We're on Win32??? ($OSTYPE)"
+# elif [[ "$OSTYPE" == "freebsd"* ]]; then
+#     echo "We're on Freebsd <3 ($OSTYPE)"
+# else
+#     echo "We're on some freaky OS I don't know ($OSTYPE)"
+# fi
+#
+# }}}
