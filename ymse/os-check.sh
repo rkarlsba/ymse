67cebecb90fb3df0389daf72203e6e333f132aea
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2024-01-29 03:25:30 +0100

    bedre med case

--- ymse/os-check.sh
+++ ymse/os-check.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-#
+# vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
+
+case "$OSTYPE" in
+    "linux-gnu"*)
+        echo "We're on Linux! ($OSTYPE)"
+        ;;
+    "darwin"*)
+        echo "We're on a mac! ($OSTYPE)"
+        ;;
+    "cygwin")
+        echo "We're on Cygwin! ($OSTYPE)"
+        ;;
+    "msys")
+        echo "We're on MinGW? ($OSTYPE)"
+        ;;
+    "win32")
+        echo "We're on Win32??? ($OSTYPE)"
+        ;;
+    "freebsd"*)
+        echo "We're on Freebsd <3 ($OSTYPE)"
+        ;;
+    *)
+        echo "We're on some freaky OS I don't know ($OSTYPE)"
+        ;;
+esac
+exit
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "We're on linux! ($OSTYPE)"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
