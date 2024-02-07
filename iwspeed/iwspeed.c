d89ac10ce15aef5c999b17fe95fc37c4438b20bc
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2024-02-07 20:19:29 +0100

    asdf

---
+++ iwspeed/iwspeed.c
@@ -0,0 +1,18 @@
+/*
+ * vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
+ *
+ * iwspeed - small test to fetch iwspeed in Hz
+ *
+ * Written by Roy Sigurd Karlsbakk <roy@karlsbakk.net> 2014-02
+ *
+ * Released under the BSD-3-Clause License
+ * See https://opensource.org/license/bsd-3-clause/ for details
+ */
+#include <iwlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int s;
+
+}
