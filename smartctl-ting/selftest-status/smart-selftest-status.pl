80d7a57d031e225b6dc3357a801ca2768c17906e
root <root@mediabox.kristshell.net>
2024-02-10 17:26:53 +0100

    funker

--- smartctl-ting/selftest-status/smart-selftest-status.pl
+++ smartctl-ting/selftest-status/smart-selftest-status.pl
@@ -5,14 +5,61 @@ use strict;
 use warnings;
 use Getopt::Long;
 
+# Docs and comments {{{
+# 
+# Self-test execution status:      (   0)   The previous self-test routine completed
+#                                           without error or no self-test has ever 
+#                                           been run.
+# 
+# Self-test execution status:      ( 241)   Self-test routine in progress...
+#                                           10% of test remaining.
+# 
+# }}}
 # Globals {{{
 
 my $program_name = $0;
-my $opt_filename = undef;
+my $opt_filenames = undef;
 my $opt_help = 0;
 my $opt_verbose = 0;
 my $verbose = undef;
 my $filename = undef;
+
+# }}}
+# sub devinfo($devname) {{{
+
+sub devinfo {
+    my $fd;
+    my $args = @_;
+    return $args if ($args lt 1);
+    my $devname = $_[0];
+    my $foundstatus = 0;
+    my $statuscode = undef;
+    my $statustext = undef;
+
+    if ( -f $devname ) {
+        open $fd,$devname || 
+            die "Error opening file \"$devname\": $!";
+    } elsif ( -b $devname ) {
+        open $fd,"smartctl -x $devname|" || 
+            die "Error opening file \"$devname\": $!";
+    }
+    while (my $s = <$fd>) {
+        chomp($s);
+        if ($foundstatus) {
+            if ($s =~ /^\s\s\s+(\w.*)/) {
+                $statustext .= " $1";
+            } else {
+                last;
+            }
+        } elsif ($s =~ /^Self-test execution status:\s+\(\s*(\d+)\s*\)\s+(.*)/) {
+            $statuscode = $1;
+            $statustext = $2;
+            $foundstatus++;
+        }
+    }
+    $statustext =~ s/\s\s+/ /g;
+    print "$devname: [$statuscode]: $statustext\n";
+}
 
 # }}}
 # sub help([errstr [, exitcode]]) {{{
@@ -27,7 +74,7 @@ sub help {
     $s .= " and help() was called with $args arguments, which is confusing" if ($args gt 2);
 
     print "ERROR: $s\n" if (defined($s));
-    print("Usage: $program_name [ -f output_from_smartctl-a.txt ] [ -h ] [ -v [ -v [ ... ]]]\n");
+    print("Usage: $program_name [ -h ] [ -v [ -v [ ... ]]] /dev/sdX [ /dev/sdY [ /dev/sdZ [ ... ] ] ] [ /file/to/smartctl-x/output.txt [ /another/output/file.txt [ ... ] ] \n");
     exit($exitcode);
 }
 
@@ -38,13 +85,16 @@ Getopt::Long::Configure('bundling');
 GetOptions(
     "v+"    => \$opt_verbose,   "verbose+"      => \$opt_verbose,
     "h"     => \$opt_help,      "help"          => \$opt_help,
-    "f=s"   => \$opt_filename,  "filename=s"    => \$opt_filename,
 ) or &help("Invalid argument");
 
 &help if ($opt_help);
 $verbose=$opt_verbose if (defined($opt_verbose));
-$filename=$opt_filename if (defined($opt_filename));
 
 # }}}
 
-&help('Stomach ache', 42);
+my $args_left = $#ARGV+1;
+&help("Got $args_left args - I need a dev or a smartctl -a output file!") if ($args_left le 0);
+
+while (my $fn = shift) {
+    &devinfo($fn);
+}
