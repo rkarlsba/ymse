aa50e8d12631dea5001b932a3b58b43181739ba7
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2024-06-04 17:10:21 +0200

    +copression++

--- postgresql/pgbackup/pg-dump.sh
+++ postgresql/pgbackup/pg-dump.sh
@@ -5,11 +5,19 @@
 # Syntax:
 #   pg-dump.sh db1 [ db2 [ db3 [ ... ]]]
 
+die() {
+    echo "$*" 1>&2
+    exit 1
+}
+
+# Backup directory
+backupdir="$HOME/backups"
+
 # Please exit on errors ;)
 set -e
 
-# Date format, here as used in filename 'zabbix-2023-10-03_13_15_15.dump'
-datefmt='%Y-%m-%d_%H_%M_%S'
+# Date format, here as used in filename 'zabbix-2023-10-03_13-15-15.dump'
+datefmt='%Y-%m-%d_%H-%M-%S'
 
 # The actual date string output by date(1)
 datestr=$( date +$datefmt )
@@ -21,15 +29,28 @@ dumpjobs=8
 # Dump format is either plain (SQL), custom (compressed native postgres
 # format), directory or tar. See pg_dump(1) for details.
 dumpformat='custom'
+compression='none'
+compcmd='cat'
+
+if [ ! -d $backupdir ]
+then
+    mkdir -p $backupdir || die "Can't make dir $backupdir: $!"
+fi
+[ -w $backupdir ] || die "Backupdir $backupdir isn't writable"
+
+cd $backupdir
 
 # Pars args
-optstr=":f:"
+optstr=":c:f:"
 
 while getopts ${optstr} opt
 do
     case ${opt} in
         f)
             dumpformat=${OPTARG}
+            ;;
+        c)
+            compression=${OPTARG}
             ;;
         :)
             echo "Option -${OPTARG} requires an argument."
@@ -66,9 +87,32 @@ case $dumpformat in
         ;;
 esac
 
+case $compression in
+    gz)
+        ext2=".gz"
+        compcmd="gzip"
+        ;;
+    xz)
+        ext2=".xz"
+        compcmd="xz"
+        ;;
+    bzip2|bz2)
+        ext2=".bz2"
+        compcmd="bzip2"
+        ;;
+    none)
+        ext2=''
+        compcmd='cat'
+        ;;
+    *)
+        echo "Dump format '$dumpformat' is unknown - giving up" >&2
+        exit 1
+        ;;
+esac
+
 # Main code
 for db in $@
 do
-    filename=$db-$datestr$ext
-    pg_dump --format=$dumpformat --jobs=$dumpjobs $db > $filename
+    filename=$db-$datestr$ext$ext2
+    pg_dump --format=$dumpformat --jobs=$dumpjobs $db | $compcmd > $filename
 done
