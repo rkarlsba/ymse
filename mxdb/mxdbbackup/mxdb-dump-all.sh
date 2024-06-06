381b0116272205d0c0089d36a8b1fc1a82fea448
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2024-06-06 12:33:21 +0200

    nytt - masse nytt

--- mxdb/mxdbbackup/mxdb-dump-all.sh
+++ mxdb/mxdbbackup/mxdb-dump-all.sh
@@ -4,17 +4,50 @@
 databases=$( echo show databases | mysql | egrep -v "^database|^lost+found|^information_schema|^performance_schema" )
 backuptarget="/var/backups/mariadb"
 mxdbdump="mariadb-dump" # or "mysqldump" or whatever comes next
-xz=0
 dbbbfname="%s-$( date "+%Y-%m-%d" ).sql"
-comp='cat'
-ext=''
+
+function show_help {
+    [ $# -gt 0 ] && echo $@
+    echo <<EOT
+Syntax: $0 [-xz|-gz|-bz2|-h]
+EOT
+}
 
 # echo /var/backups/$( date "+%Y/%m/%d/%H/%M/%S" )/synopsis.sql.xz
 
-if [ "$1"="-xz" ]
-then
-    xz=1
-fi
+case $# in
+    0)
+        comp='cat'
+        ext=''
+        ;;
+    1)
+        case $1 in
+            '-xz')
+                comp='xz'
+                ext='.xz'
+                ;;
+            '-gz')
+                comp='gz'
+                ext='.gz'
+                ;;
+            '-bz'|'-bz2')
+                comp='bz2'
+                ext='.bz2'
+                ;;
+            '-h'|'--help')
+                show_help
+                ;;
+            *)
+                echo "Unknown/invalid argument '$1' - exiting" >&2
+                exit 1
+                ;;
+        esac
+        ;;
+    *)
+        echo "Invalid number of arguments - exiting" >&2
+        exit 2
+        ;;
+esac
 
 [ -d $backuptarget ] || mkdir -p $backuptarget
 
@@ -24,7 +57,6 @@ do
     if [ $xz -gt 0 ]
     then
         comp='xz -c'
-        ext='.xz'
     fi
     mysqldump $db | $comp > $backuptarget/$db.sql$ext
 done
