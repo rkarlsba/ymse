7a0da067f3ddace0c17ebe055cffcdd6d937cf00
root <root@synopsis01.oslomet.no>
2024-06-06 12:58:53 +0200

    ok?

--- mxdb/mxdbbackup/mxdb-dump-all.sh
+++ mxdb/mxdbbackup/mxdb-dump-all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # vim:ts=4:sw=4:sts=4:et:ai
 
-databases=$( echo show databases | mysql | egrep -v "^database|^lost+found|^information_schema|^performance_schema" )
+databases=$( echo show databases | mysql | egrep -v "^Database|^lost+found|^information_schema|^performance_schema" )
 backuptarget="/var/backups/mariadb"
 mxdbdump="mariadb-dump" # or "mysqldump" or whatever comes next
 dbbbfname="%s-$( date "+%Y-%m-%d" ).sql"
+comp='cat'
+ext=''
+force=0
 
 function show_help {
     [ $# -gt 0 ] && echo $@
@@ -13,50 +16,54 @@ Syntax: $0 [-xz|-gz|-bz2|-h]
 EOT
 }
 
-# echo /var/backups/$( date "+%Y/%m/%d/%H/%M/%S" )/synopsis.sql.xz
+# Parse arguments first
+optstr=":bfghx?"
 
-case $# in
-    0)
-        comp='cat'
-        ext=''
-        ;;
-    1)
-        case $1 in
-            '-xz')
-                comp='xz'
-                ext='.xz'
-                ;;
-            '-gz')
-                comp='gz'
-                ext='.gz'
-                ;;
-            '-bz'|'-bz2')
-                comp='bz2'
-                ext='.bz2'
-                ;;
-            '-h'|'--help')
-                show_help
-                ;;
-            *)
-                echo "Unknown/invalid argument '$1' - exiting" >&2
-                exit 1
-                ;;
-        esac
-        ;;
-    *)
-        echo "Invalid number of arguments - exiting" >&2
-        exit 2
-        ;;
-esac
+while getopts ${optstr} opt
+do
+    case ${opt} in
+        'b')
+            comp='bzip2 -c'
+            ext='.bz2'
+            ;;
+        'f')
+            force=1
+            ;;
+        'g')
+            comp='gzip -c'
+            ext='.gz'
+            ;;
+        'x')
+            comp='xz -c'
+            ext='.xz'
+            ;;
+        'h'|'?')
+            show_help
+            ;;
+        :)
+            echo "Option -${OPTARG} requires an argument."
+            exit 1
+            ;;
+        *)
+            echo "Invalid option: -${OPTARG}."
+            exit 2
+            ;;
+    esac
+done
+shift $( expr $OPTIND - 1 )
 
 [ -d $backuptarget ] || mkdir -p $backuptarget
 
 for db in $databases
 do
     printf -v filename "$backuptarget/$dbbbfname" $db
-    if [ $xz -gt 0 ]
+    if [ -f $filename ]
     then
-        comp='xz -c'
+        if [ $force -le 0 ]
+        then
+            echo "File $filename exists and -f isn't given - skipping"
+            continue
+        fi
     fi
-    mysqldump $db | $comp > $backuptarget/$db.sql$ext
+    echo "$mxdbdump $db | $comp > $backuptarget/$db.sql$ext"
 done
