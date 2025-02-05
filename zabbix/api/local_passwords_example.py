dbabee59b92f2e5b7e39f262b64921660db396e9
root <root@zabbix-prod01.oslomet.no>
2025-02-05 11:16:07 +0100

    mere

--- zabbix/api/local_passwords_example.py
+++ zabbix/api/local_passwords_example.py
@@ -6,16 +6,16 @@
 # ut data og bruker bare api_user etc, ikke api_src_ eller api_dst_
 
 # Common user for normal use
+api_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
 api_user = 'zabbix-api-user'
 api_password = 'supersecret'
-api_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
 
 # User used for migration - source part
+api_src_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
 api_src_user = 'zabbix-api-user'
 api_src_password = 'supersecret'
-api_src_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
 
 # User used for migration - destination part
+api_dst_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
 api_dst_user = 'zabbix-api-user'
 api_dst_password = 'supersecret'
-api_dst_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
