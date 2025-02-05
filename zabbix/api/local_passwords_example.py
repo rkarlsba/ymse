77e89a83c1789f5609aa6b66051856ac5ed3c735
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2025-02-05 09:53:09 +0100

    ta med eksempelfil til passord, da

---
+++ zabbix/api/local_passwords_example.py
@@ -0,0 +1,16 @@
+# Sample password file
+
+# Common user for normal use
+api_user = 'zabbix-api-user'
+api_password = 'supersecret'
+api_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
+
+# User used for migration - source part
+api_src_user = 'zabbix-api-user'
+api_src_password = 'supersecret'
+api_src_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
+
+# User used for migration - destination part
+api_dst_user = 'zabbix-api-user'
+api_dst_password = 'supersecret'
+api_dst_url = 'https://zabbix.my.tld/zabbix/api_jsonrpc.php';
