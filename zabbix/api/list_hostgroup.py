c551e3d708dc7f26448c165fc8a544c1a625f547
root <root@zabbix-prod01.oslomet.no>
2025-02-05 15:51:47 +0100

    mere

--- zabbix/api/list_hostgroup.py
+++ zabbix/api/list_hostgroup.py
@@ -25,20 +25,15 @@ if __name__ == "__main__":
 
     argparser = argparse.ArgumentParser(add_help=builtinhelp)
 
-    argparser.add_argument("-H", "--hostgroup", type=str, help="List members of given hostgroup")
+    argparser.add_argument("-H", "--hostgroup", type=str, help="List members of given hostgroup", required=True)
     argparser.add_argument("-S", "--server", type=str, help=f"Zabbix Server URL, for instance https://my.zabbixsrv.tld/zabbix/api_jsonrpc.php")
     argparser.add_argument("-U", "--user", type=str, help="Username")
     argparser.add_argument("-P", "--password", type=str, help="Passsword")
-    # argparser.add_argument("-F", "--password-file", type=str, help="Path to passsword file with user:s3Cr3t")
 
     if not builtinhelp:
         argparser.add_argument("-h", "--help", action="store_true", help="Show this help message and exit")
 
     args = argparser.parse_args()
-
-    if args.hostgroup == None:
-        print("We need a hostgroup");
-        exit(1)
 
     hostgroup = args.hostgroup
 
