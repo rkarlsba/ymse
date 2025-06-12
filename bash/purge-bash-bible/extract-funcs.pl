c51718dd917fe45ef34a678f627f0a64c4bd4c24
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2025-06-12 08:36:44 +0200

    bla

--- bash/purge-bash-bible/extract-funcs.pl
+++ bash/purge-bash-bible/extract-funcs.pl
@@ -5,9 +5,19 @@ my $fn = 'pure-bash-bible.md';
 open(my $fh, '<', $fn)
     or die "Can't open < $fn $!";
 
-# Process every line in input.txt
+my $found=0;
 while (my $line = readline($fh)) {
     chomp($line);
-    print($line);
+    if ($found) {
+        if ($line =~ /^```$/) {
+            $found=0;
+            continue;
+        } else {
+            print("$line\n");
+        }
+    } elsif ($line =~ /^```sh$/) {
+        $found=1;
+        print("$line\n");
+    }
 }
 
