43897f9e75fd52fbe875a005a43e9a510272b2c7
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2025-06-17 13:46:12 +0200

    mere

--- zabbix/plugins/check-https/check_https.py
+++ zabbix/plugins/check-https/check_https.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python
+# vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
+# {{{ gammalt
+# import argparse
+# import http.client
+# import sys
+# 
+# def main():
+#     parser = argparse.ArgumentParser(description='HTTPS checker for all addresses')
+#     parser.add_argument('host', help='The target HTTPS hostname (www.example.com)')
+#     parser.add_argument('--path', default='/', help='Path to request (default: /)')
+#     args = parser.parse_args()
+# 
+#     conn = http.client.HTTPSConnection(args.host)
+#     conn.request("GET", args.path)
+#     response = conn.getresponse()
+# 
+#     print("Status:", response.status)
+#     print("Reason:", response.reason)
+#     print("Body:", response.read().decode())
+# 
+#     conn.close()
+#     sys.exit(0)
+# 
+# if __name__ == "__main__":
+#     main()
+# }}}
 
 import argparse
 import http.client
-import sys
+import ssl
+import certifi
 
 def main():
-    parser = argparse.ArgumentParser(description='HTTPS checker for all addresses')
-    parser.add_argument('host', help='The target HTTPS hostname (www.example.com)')
+    parser = argparse.ArgumentParser(description='HTTPS client using certifi CA bundle')
+    parser.add_argument('host', help='The target HTTPS server (e.g., www.example.com)')
     parser.add_argument('--path', default='/', help='Path to request (default: /)')
     args = parser.parse_args()
 
-    conn = http.client.HTTPSConnection(args.host)
+    # Use certifi's CA bundle
+    context = ssl.create_default_context(cafile=certifi.where())
+
+    conn = http.client.HTTPSConnection(args.host, context=context)
     conn.request("GET", args.path)
     response = conn.getresponse()
 
@@ -19,8 +49,7 @@ def main():
     print("Body:", response.read().decode())
 
     conn.close()
-    sys.exit(0)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
