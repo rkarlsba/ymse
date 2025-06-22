d4d7d16ac0537ac9e0f376a46fdcca3a3a7a5511
rkarlsba <roy@karlsbakk.net>
2025-06-22 19:10:56 +0200

    funker

--- find-nc-encrypted-files/find-nc-encrypted-files.py
+++ find-nc-encrypted-files/find-nc-encrypted-files.py
@@ -5,6 +5,10 @@ import magic
 import os
 import requests
 import sys
+from pathvalidate import sanitize_filepath
+
+def is_valid_path(file_path):
+    return file_path == sanitize_filepath(file_path)
 
 def check_file_contents(file):
     mime = magic.Magic(mime=True)
@@ -14,25 +18,49 @@ def check_file_contents(file):
         content = f.read(1024)  # Read the first 1KB; usually enough for magic detection
 
     # Get MIME type
-    print(mime.from_buffer(content))
+    # print(mime.from_buffer(content))
 
     # Get human-readable description (like the file command)
-    print(file_type.from_buffer(content))
+    # print(file_type.from_buffer(content))
+    return file_type.from_buffer(content)
 
-def find_nc_encrypted_files(path, oce):
+def file_is_oc_encrypted(file):
     # HBEGIN:oc_encryption_module:OC_DEFAULT_MODULE:cipher:AES-256-CTR:signed:true:HEND
-    signature = 'HBEGIN:oc_encryption_module'
+    os_enc_signature = b'HBEGIN:oc_encryption_module'
+    with open(file, 'rb') as f:
+        content = f.read(1024)  # Read the first 1KB; usually enough for magic detection
+    return content.startswith(os_enc_signature)
 
-    for dirpath, dirnames, filenames in os.walk(path):
-        for filename in filenames:
-            if filename.startswith(signature):
-                print(os.path.join(dirpath, filename))
+
+def find_nc_encrypted_files(path, oc_encrypted=False):
+
+    try:
+        filetype = ''
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                fullfilename = os.path.join(dirpath, filename)
+                if (oc_encrypted):
+                    if file_is_oc_encrypted(fullfilename):
+                        filetype = 'Encrypted by Owncloud/Nextcloud'
+                    else:
+                        next
+                else:
+                    filetype = check_file_contents(fullfilename)
+                print(f"{fullfilename}: {filetype}")
+    except KeyboardInterrupt:
+        print("Exiting cleanly.")
+        sys.exit(0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Search for files encrypted by nextcloud')
     parser.add_argument('path', type=str, help='Which path to start looking for files')
     parser.add_argument('-e', '--oc-encrypted', action='store_true', help='Only find files encryped by Owncloud/Nextcloud')
+
     args = parser.parse_args()
 
-    find_nc_encrypted_files(args.path)
+    if not os.path.isdir(args.path):
+        print(f"ERROR: {args.path} is not a dir")
+        sys.exit(1)
+
+    find_nc_encrypted_files(args.path, args.oc_encrypted)
 
