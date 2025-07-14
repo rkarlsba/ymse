67d98f4a7b8159c42daa93423fd6feba73e0e664
root <root@zabbix-prod01.oslomet.no>
2025-07-14 18:30:27 +0200

    da funker js

--- zabbix/api/list_hostgroup.py
+++ zabbix/api/list_hostgroup.py
@@ -138,20 +138,78 @@ if __name__ == "__main__":
                 text-align: left;
             }
         </style>'''
+                js = '''
+<script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("hosttable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>'''
                 if firstline == 0:
                     output = f'''<html>
     <head>
         <title>Maskiner i hostgroup {args.hostgroup}</title>
         {css}
+        {js}
     </head>
     <body>
         <h1>Maskiner i hostgroup <b>{args.hostgroup}</b></h1>
         <p>
-        <table>
+        <table id="hosttable">
             <tr>
-                <th>hostid</th>
-                <th>hostname</th>
-                <th>disabled</th>
+                <th onclick="sortTable(0)">hostid</th>
+                <th onclick="sortTable(1)">hostname</th>
+                <th onclick="sortTable(2)">disabled</th>
             </tr>\n'''
                     firstline=1
                 output += f'''            <tr>
