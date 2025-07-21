3a435923f91dbe73f1dafad7066bba14ed09808b
Roy Sigurd Karlsbakk <roy@karlsbakk.net>
2025-07-21 07:43:42 +0200

    dah

--- zabbix/plugins/check-ldap/zabbix_check_ldap.py
+++ zabbix/plugins/check-ldap/zabbix_check_ldap.py
@@ -3,23 +3,41 @@
 import sys
 import ldap
 
-Server = "ldaps://openldap-prod01.oslomet.no/"
-DN, Secret, un = sys.argv[1:4]
+def get_user_pass(password_file):
+    try:
+        f=open(password_file,"r")
+        lines=f.readlines()
+        for line in lines:
+            line = re.sub(r"[\r\n]", "", line)
+            auth_tokens = line.split(':')
+        f.close()
 
-opt_basedn="ou=tilsatt,ou=oslomet,dc=oslomet,dc=no";
-Scope = ldap.SCOPE_SUBTREE
-Filter = "(&(objectClass=user)(sAMAccountName="+un+"))"
-Attrs = ["displayName"]
+        print(f"{auth_token[0]}:{auth_token[0]}")
 
-l = ldap.initialize(Server)
-l.protocol_version = 3
-print l.simple_bind_s(DN, Secret)
+    except Exception as e:
+        logging.error(traceback.format_exc())
 
-r = l.search(Base, Scope, Filter, Attrs)
-Type,user = l.result(r,60)
-Name,Attrs = user[0]
-if hasattr(Attrs, 'has_key') and Attrs.has_key('displayName'):
-  displayName = Attrs['displayName'][0]
-  print displayName
+def main(): 
+    Server = "ldaps://openldap-prod01.oslomet.no/"
+    DN, Secret, un = sys.argv[1:4]
 
-sys.exit()
+    opt_basedn="ou=tilsatt,ou=oslomet,dc=oslomet,dc=no";
+    Scope = ldap.SCOPE_SUBTREE
+    Filter = "(&(objectClass=user)(sAMAccountName="+un+"))"
+    Attrs = ["displayName"]
+
+    l = ldap.initialize(Server)
+    l.protocol_version = 3
+    print l.simple_bind_s(DN, Secret)
+
+    r = l.search(Base, Scope, Filter, Attrs)
+    Type,user = l.result(r,60)
+    Name,Attrs = user[0]
+    if hasattr(Attrs, 'has_key') and Attrs.has_key('displayName'):
+      displayName = Attrs['displayName'][0]
+      print displayName
+
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
